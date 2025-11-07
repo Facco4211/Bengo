b37a7f692374738f8c646869d568952078b0b813
Facco4211 <rafaelsoaresfacco@gmail.com>
2025-11-06 21:20:33 -0300

    Carrossel mais funcional

--- src/components/Carrossel/index.jsx
+++ src/components/Carrossel/index.jsx
@@ -8,26 +8,29 @@ import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 
 export default function Carrossel() {
   const slides = [
-        {
-      marcacao: 'Aventura',marcacao2: 'RPG',
-      titulo: 'Disco Elysium - The Final Cut',
+    {
+      marcacao: 'Aventura', marcacao2: 'RPG',
+      titulo: 'Disco Elysium',
       descricao:
-        '',
+        'Disco Elysium é um RPG onde você interpreta um detetive que precisa investigar um assassinato enquanto tenta juntar as peças de sua própria vida.',
       background: Disco,
+      preco: "R$165,00",
     },
     {
-      marcacao: 'Ação',marcacao2: 'Aventura',
+      marcacao: 'Ação', marcacao2: 'Aventura',
       titulo: 'Far Cry 3',
       descricao:
-        '',
+        'Disco Elysium é um RPG onde você interpreta um detetive que precisa investigar um assassinato enquanto tenta juntar as peças de sua própria vida.',
       background: FarCry3,
+      preco: "R$90,00",
     },
     {
-      marcacao: 'Aventura',marcacao2: 'Fantasia',
+      marcacao: 'Aventura', marcacao2: 'Fantasia',
       titulo: 'Ninja',
       descricao:
-        '',
+        'Disco Elysium é um RPG onde você interpreta um detetive que precisa investigar um assassinato enquanto tenta juntar as peças de sua própria vida.',
       background: Ninja,
+      preco: "R$75,00",
     },
   ];
 
@@ -55,22 +58,23 @@ export default function Carrossel() {
       <div className={styles.carrosselSlide} style={{ background: `URL(${slides[indiceAtual].background}) no-repeat center / cover ` }}>
         <div className={styles.carrosselTexto}>
           <div className={styles.marcacoes}>
-          <strong>{slides[indiceAtual].marcacao}</strong>
-          <strong>{slides[indiceAtual].marcacao2}</strong>
+            <strong>{slides[indiceAtual].marcacao}</strong>
+            <strong>{slides[indiceAtual].marcacao2}</strong>
           </div>
           <h2>{slides[indiceAtual].titulo}</h2>
-          {/* <p>{ slides[indiceAtual].descricao}</p> */}
-          <a href="/games" >
-            Comprar
-          </a>
+          <p>{slides[indiceAtual].descricao}</p>
+          <div className={styles.comprar}>
+            <a href="/games" >Comprar</a>
+            <p>{slides[indiceAtual].preco}</p>
+          </div>
         </div>
       </div>
       <div className={styles.controlesCarrossel}>
         <button className={styles.btnAnterior} onClick={(irParaAnterior)} >
-          <FaChevronLeft color='#ffffffff' size={60} />
+          <FaChevronLeft color='#4A72D8' size={30} />
         </button>
         <button className={styles.btnProximo} onClick={irParaProximo} >
-          <FaChevronRight color='#ffffffff' size={60} />
+          <FaChevronRight color='#4A72D8' size={30} />
         </button>
       </div>
     </div>
